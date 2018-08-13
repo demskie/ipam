5b826388eddd538a2c2132c4335d8a8a01fd5bd0
demskie <alex.demskie@gmail.com>
2018-08-12 23:11:56 -0700

    add a results channel to the webserver invocation

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/demskie/ipam/server"
-
 func main() {
-	ipam := server.NewIPAMServer()
+	//ipam := server.NewIPAMServer()
 }
--- server/server.go
+++ server/server.go
@@ -18,32 +18,44 @@ const (
 	defaultTimeLayout = "01-02-2006 15:04:05"
 )
 
+// MutatedData contains the raw lines of the changed subnets.csv and history.txt files
+type MutatedData struct {
+	subnets []string
+	history []string
+}
+
 // IPAMServer is the object used to mutate and read data
 type IPAMServer struct {
-	subnets     *subnets.Tree
-	history     *history.UserActions
-	debug       *history.ServerLogger
-	dns         *dns.Bucket
-	pinger      *ping.Pinger
-	mutationMtx *sync.Mutex
+	subnets      *subnets.Tree
+	history      *history.UserActions
+	debug        *history.ServerLogger
+	dns          *dns.Bucket
+	pinger       *ping.Pinger
+	mutationMtx  *sync.Mutex
+	mutationChan chan MutatedData
 }
 
 // NewIPAMServer returns a new server object
 func NewIPAMServer() *IPAMServer {
 	return &IPAMServer{
-		subnets:     subnets.NewTree(),
-		history:     history.NewUserActions(),
-		debug:       history.NewServerLogger(),
-		dns:         dns.NewBucket(),
-		pinger:      ping.NewPinger(),
-		mutationMtx: &sync.Mutex{},
+		subnets:      subnets.NewTree(),
+		history:      history.NewUserActions(),
+		debug:        history.NewServerLogger(),
+		dns:          dns.NewBucket(),
+		pinger:       ping.NewPinger(),
+		mutationMtx:  &sync.Mutex{},
+		mutationChan: make(chan MutatedData, 1),
 	}
 }
 
-// Serve will start the HTTP Webserver
-func (ipam *IPAMServer) Serve(addr string, debug bool) {
-	go startDebugServer(debug)
-	ipam.startWebServer(addr)
+// ServeAndReceiveChan will start the HTTP Webserver and stream results back to the caller
+func (ipam *IPAMServer) ServeAndReceiveChan(addr string, debug bool) (changes chan MutatedData) {
+	go func() {
+		go startDebugServer(debug)
+		ipam.startWebServer(addr)
+		close(ipam.mutationChan)
+	}()
+	return ipam.mutationChan
 }
 
 func startDebugServer(debug bool) {
